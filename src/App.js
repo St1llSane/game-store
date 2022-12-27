d02a50d9926e4b8802dca880369b8a5af8809584
Alexander Vetrov <notescape262@gmail.com>
2022-12-27 15:57:11 +0300

    cart without mockapi

--- src/App.js
+++ src/App.js
@@ -12,17 +12,10 @@ function App() {
   const [gamesByGenres, setGamesByGenres] = useState([])
   const [cartGames, setCartGames] = useState([])
 
-  // TODO Добавить обозначение, что игра находится в корзине
+  // Добавить сохрание корзины в LocalStorage и сохранить состояние кнопок после перезагрузки страницы
 
   useEffect(() => {
     async function fetchingData() {
-      await axios
-        .get('https://639df5493542a2613053e993.mockapi.io/cartGames')
-        .catch((error) => {
-          alert('Ошибка при загрузке корзины')
-          console.log(error)
-        })
-        .then((res) => setCartGames(res.data))
       await axios
         .get('https://639df5493542a2613053e993.mockapi.io/games')
         .catch((error) => {
@@ -51,26 +44,10 @@ function App() {
 
   const addGameToCartHandler = (item) => {
     const thisGame = cartGames.find((game) => +game.parentId === +item.id)
-    item.inCart = !item.inCart
-    console.log('item', item)
 
     if (thisGame) {
-      axios
-        .delete(
-          `https://639df5493542a2613053e993.mockapi.io/cartGames/${thisGame.id}`
-        )
-        .catch((error) => {
-          alert('Ошибка при удалении из корзины')
-          console.log(error)
-        })
       setCartGames((prev) => prev.filter((game) => +game.parentId !== +item.id))
     } else {
-      axios
-        .post('https://639df5493542a2613053e993.mockapi.io/cartGames', item)
-        .catch((error) => {
-          alert('Ошибка при добавлении в корзину')
-          console.log(error)
-        })
       setCartGames([...cartGames, item])
     }
   }
@@ -80,20 +57,9 @@ function App() {
   }
 
   const deleteGameFromCart = (id) => {
-    // TODO Пофиксить баг, попробовать сделать через parentId
-
-    // const thisGame = cartGames.find((game) => +game.parentId === +id)
-    // thisGame.inCart = !thisGame.inCart
-    axios
-      .delete(`https://639df5493542a2613053e993.mockapi.io/cartGames/${id}`)
-      .catch((error) => {
-        alert('Ошибка при удалении из корзины')
-        console.log(error)
-      })
     setCartGames((prev) => prev.filter((game) => +game.id !== +id))
   }
 
-  console.log('games', games)
   console.log('cartGames', cartGames)
 
   return (
